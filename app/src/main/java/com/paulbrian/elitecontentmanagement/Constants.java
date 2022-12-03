14e2fff1a8658c4f03e873700b5324395f85c24b
Paul-Brian-17 <paulbrian254@gmail.com>
2022-12-03 18:22:26 +0300

    comment like and share content

---
+++ app/src/main/java/com/paulbrian/elitecontentmanagement/Constants.java
@@ -0,0 +1,7 @@
+package com.paulbrian.elitecontentmanagement;
+
+public class Constants {
+    public static final String[] categories = { "Education", "Kids", "Religious", "Entertainment", "Sports", "LifeStyle", "News"};
+    public static final String SERVER_BASE_URL_GENERAL_APIS = "https://final-year-project-200.herokuapp.com/general-apis";
+
+}
